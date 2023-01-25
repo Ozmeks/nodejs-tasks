083e27a502d802fd35a8d0368d960d9c895ef3a8
ozmeks <ozmeks@yandex.ru>
2023-01-25 21:16:47 +0100

    add weather cli

---
+++ 8-weather-extended/services/api.service.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const getIcon = (icon) => {
+	switch (icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+};
+
+const getWeather = async (token, city, lang) => {
+	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		params: {
+			q: city,
+			appid: token,
+			lang: lang,
+			units: 'metric'
+		}
+	});
+	return data;
+};
+
+export { getWeather, getIcon };
