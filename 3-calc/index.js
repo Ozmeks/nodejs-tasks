09a5492b40873ed39e5abfdd4d95cf95559199ff
Oleg Ozhegov <oo@webmag.io>
2026-02-28 15:24:36 +0100

    Add 3-calc folder

---
+++ 3-calc/index.js
@@ -0,0 +1,25 @@
+import add from './add.js';
+import multiply from './multiply.js';
+import subtract from './subtract.js';
+import divide from './divide.js';
+
+const mapping = {
+  add,
+  subtract,
+  multiply,
+  divide,
+};
+
+const calc = (num1, num2, operation) => {
+  if (!mapping[operation]) {
+    console.log(
+      'Operation does not exist. Avaliable: add, subtract, multiply, divide.'
+    );
+    return;
+  }
+  const result = mapping[operation](Number(num1), Number(num2));
+  console.log(result);
+};
+
+const [, , num1, num2, operation] = process.argv;
+calc(num1, num2, operation);
