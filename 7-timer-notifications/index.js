6c54c4e9a2cf3d2c9d22368f8cbcfe68114a8578
Ozmeks <omehryakov@gmail.com>
2026-03-06 15:42:09 +0100

    Add folder 7-timer-notifications

---
+++ 7-timer-notifications/index.js
@@ -0,0 +1,27 @@
+import notifier from "node-notifier";
+
+const timer = (time) => {
+  const indexMappingToMillisecs = {
+    0: 1000, // Seconds -> Milliseconds
+    1: 60000, // Minutes -> Milliseconds
+    2: 3600000 // Hours -> Milliseconds
+  };
+
+  const milliseconds = time.split(' ')
+    .reverse()
+    .map((item) => Number(item.slice(0, -1)))
+    .reduce((acc, item, index) => acc + item * indexMappingToMillisecs[index], 0);
+
+  setTimeout(() => {
+    notifier.notify(
+      {
+        title: "Timer",
+        message: "The timer rang.",
+        sound: true,
+      }
+    );
+  }, milliseconds);
+};
+
+const [, , ] = process.argv;
+timer(time);
