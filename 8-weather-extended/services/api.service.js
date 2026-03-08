7a4aa6b0d4eaabc061cfdd510e3010f91480c301
Oleg Ozhegov <oo@webmag.io>
2026-03-08 13:05:21 +0100

    Add folder 8-weather-extended

---
+++ 8-weather-extended/services/api.service.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+
+const getIcon = (icon) => {
+	switch (icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+};
+
+const getWeather = async (city) => {
+	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
+	if (!token) {
+		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
+	}
+	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		params: {
+			q: city,
+			appid: token,
+			lang: 'ru',
+			units: 'metric'
+		}
+	});
+	return data;
+};
+
+export { getWeather, getIcon };
---
+++ 8-weather-extended/services/log.service.js
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = (error) => {
+	console.log(chalk.bgRed(' ERROR ') + ' ' + error);
+};
+
+const printSuccess = (message) => {
+	console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+};
+
+const printHelp = () => {
+	console.log(
+		dedent`${chalk.bgCyan(' HELP ')}
+		Без параметров - вывод погоды
+		-s [CITY] для установки города
+		-h для вывода помощи
+		-t [API_KEY] для сохранения токена
+		`
+	);
+};
+
+const printWeather = (res, icon) => {
+	console.log(
+		dedent`${chalk.bgYellow(' WEATHER ')} Погода в городе ${res.name}
+		${icon}  ${res.weather[0].description}
+		Температура: ${res.main.temp} (ощущается как ${res.main.feels_like})
+		Влажность: ${res.main.humidity}%
+		Скорость ветра: ${res.wind.speed}
+		`
+	);
+};
+
+export { printError, printSuccess, printHelp, printWeather };
